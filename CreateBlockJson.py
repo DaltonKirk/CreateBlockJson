194dad2126ce5dc8d615cc493a8ca68471464104
Dalton Kirk <daltonkirk5@gmail.com>
2018-12-27 11:30:40 +0000

    Refactored - made indent parameter on jsonEnd(indent) optional

--- CreateBlockJson.py
+++ CreateBlockJson.py
@@ -4,7 +4,7 @@ import os
 def jsonStart():
     return "{\n"
 
-def jsonEnd(indent):    
+def jsonEnd(indent = 0):    
     return "\n" + jsonIndent(indent) + "}"
 
 def jsonIndent(amount):
@@ -44,7 +44,7 @@ def blockStatesFile():
     data += jsonKey("variants", 1) + jsonStart()
     data += jsonKey("normal", 2) + "{ " + jsonKeyValue("model", modid + ":" + blockName) + "}"
     data += jsonEnd(1)
-    data += jsonEnd(0)
+    data += jsonEnd()
     return data
 
 def modelsItemFile():
@@ -53,7 +53,7 @@ def modelsItemFile():
     data += jsonKey("textures", 1) + jsonStart()
     data += jsonKeyValue("layer0", modid + ":items/" + blockName, 2)
     data += jsonEnd(1)
-    data += jsonEnd(0)
+    data += jsonEnd()
     return data
 
 def modelsBlockFile():
@@ -62,7 +62,7 @@ def modelsBlockFile():
     data += jsonKey("textures", 1) + jsonStart()
     data += jsonKeyValue("all", modid + ":blocks/" + blockName, 2)
     data += jsonEnd(1)
-    data += jsonEnd(0)
+    data += jsonEnd()
     return data
 
 # Run Script
